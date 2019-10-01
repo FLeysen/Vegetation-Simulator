68bc60b66e6ae791e043b732732b407b6fcbd4a9
Floris Leysen <floris.leysen@student.howest.be>
2019-10-01 09:10:13 +0200

    Initial commit

---
+++ Assets/Scripts/Environment/TrackAndPassTime.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackAndPassTime : MonoBehaviour
+{
+    public int ElapsedDays { get; private set; } = 0;
+
+    public void PassDay()
+    {
+        ++ElapsedDays;
+    }
+}
