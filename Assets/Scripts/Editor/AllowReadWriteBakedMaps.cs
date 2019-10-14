46c647d1800d87deb7ad3a97920be496ba3f2690
Grudar <floris.leysen@student.howest.be>
2019-10-14 16:34:47 +0200

    Added calculation of shadow on raycast location

---
+++ Assets/Scripts/Editor/AllowReadWriteBakedMaps.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+class AllowReadWriteBakedMaps : AssetPostprocessor
+{
+    void OnPreprocessTexture()
+    {
+        if (assetPath.Contains("Lightmap-"))
+        {
+            TextureImporter textureImporter = (TextureImporter)assetImporter;
+            textureImporter.isReadable = true;
+        }
+    }
+}
--- Assets/Scripts/Utilities/LightShadowMapTester.cs
+++ Assets/Scripts/Utilities/LightShadowMapTester.cs
@@ -6,9 +6,35 @@ public class LightShadowMapTester : MonoBehaviour
 {
     [SerializeField] private Transform _indicator = null;
     private Vector3 _fallbackPos = Vector3.zero;
+    private List<int> _lightCounts = new List<int>(); //This is NOT 0-4, but a bitmask (0 - 15)! It is assumed that lights will always only be inserted from r-a
+    private float _oneThird = 1f / 3f;
 
     private void Start()
     {
+        int value = 0;
+        foreach(LightmapData lightmap in LightmapSettings.lightmaps)
+        {
+            
+            foreach (Color32 pixel in lightmap.shadowMask.GetPixels32())
+            {
+                if (pixel.a == 0) { }
+                else value = value | 8;
+
+                if (pixel.r == 0) { }
+                else value = value | 1;
+
+                if (pixel.g == 0) { }
+                else value = value | 2;
+
+                if (pixel.b == 0) { }
+                else value = value | 4;
+
+                if (value != 15) continue;
+                break;
+            }
+            _lightCounts.Add(value);
+            value = 0;
+        }
         _fallbackPos = _indicator.position;
     }
 
@@ -20,12 +46,30 @@ public class LightShadowMapTester : MonoBehaviour
             _indicator.position = hitInfo.point;
             Vector2 coord = hitInfo.lightmapCoord;
             int lightIdx = hitInfo.collider.GetComponent<MeshRenderer>().lightmapIndex;
-            Color color = LightmapSettings.lightmaps[lightIdx].shadowMask.GetPixelBilinear(coord.x, coord.y);
-            Debug.Log(color);
+            Texture2D tex = LightmapSettings.lightmaps[lightIdx].shadowMask;
+            coord *= tex.height;
+            Color color = tex.GetPixel((int)coord.x, (int)coord.y);
+            Debug.Log(1 - CalculateAverageShadow(color, lightIdx));
         }
         else
         {
             _indicator.position = _fallbackPos;
         }
+    }
+
+    private float CalculateAverageShadow(Color color, int idx)
+    {
+        int val = _lightCounts[idx];
+        if (val < 2)
+            return color.r;
+        
+        else if (val < 4)
+            return (color.g + color.r) * 0.5f;
+        
+        else if (val < 8)
+            return (color.b + color.g + color.r) * _oneThird;
+        
+        else
+            return (color.b + color.g + color.r + color.a) * 0.25f;
     }
 }
